bb653befc35bfa149ade0e1e32a78e530d277171
Hayden <13798452724@163.com>
2020-08-15 11:25:58 +0800

    提交代码

--- app/src/main/java/com/populstay/populife/base/OnFragmentVisibilityChangedListener.java
+++
@@ -1,9 +0,0 @@
-package com.populstay.populife.base;
-
-/**
- * Created by Jerry
- */
-public interface OnFragmentVisibilityChangedListener {
-
-	void onFragmentVisibilityChanged(boolean visible);
-}
--- app/src/main/java/com/populstay/populife/net/callback/IFailure.java
+++
@@ -1,9 +0,0 @@
-package com.populstay.populife.net.callback;
-
-/**
- * Created by Jerry
- */
-public interface IFailure {
-
-	void onFailure();
-}
